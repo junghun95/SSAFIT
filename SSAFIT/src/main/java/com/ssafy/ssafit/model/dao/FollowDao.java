50d8a3e9031077784893d8c599bdcdc275fd39d9
Garo-o <hjs8482@naver.com>
2022-05-18 01:33:51 +0900

    update: follow-dao

--- SSAFIT/src/main/java/com/ssafy/ssafit/model/dao/FollowDao.java
+++ SSAFIT/src/main/java/com/ssafy/ssafit/model/dao/FollowDao.java
@@ -1,6 +1,6 @@
 package com.ssafy.ssafit.model.dao;
 
-public interface FallowDao {
+public interface FollowDao {
 	public void insert(String from, String to);
 	public void delete(String from, String to);
 }
