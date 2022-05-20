cc0476118d8e9ae09b3aed06e772a592428fa70f
Garo-o <hjs8482@naver.com>
2022-05-20 18:00:22 +0900

    update: refactoring dto

--- SSAFIT/src/main/java/com/ssafy/ssafit/model/dto/response/VideoResponseDTO.java
+++ SSAFIT/src/main/java/com/ssafy/ssafit/model/dto/response/VideoResponseDTO.java
@@ -1,5 +1,7 @@
 package com.ssafy.ssafit.model.dto.response;
 
+import com.ssafy.ssafit.model.dto.VideoDTO;
+
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -13,4 +15,12 @@ public class VideoResponseDTO {
 	private String videoId;
 	private String title;
 	private String part;
+	
+	public static VideoResponseDTO of(VideoDTO videoDTO) {
+		return VideoResponseDTO.builder()
+				.videoId(videoDTO.getId())
+				.title(videoDTO.getTitle())
+				.part(videoDTO.getPart().getName())
+				.build();
+	}
 }
