a349556be4dd7ec8b507c41fc08f41568dae94db
Garo-o <hjs8482@naver.com>
2022-05-20 12:40:15 +0900

    feature: board-service

---
+++ SSAFIT/src/main/java/com/ssafy/ssafit/model/service/BoardService.java
@@ -0,0 +1,16 @@
+package com.ssafy.ssafit.model.service;
+
+import java.util.List;
+import java.util.Map;
+
+import com.ssafy.ssafit.model.dto.BoardDTO;
+
+public interface BoardService {
+	public void write(BoardDTO boardDTO);
+	public void remove(int id);
+	public void modify(BoardDTO boardDTO);
+	
+	public BoardDTO read(int id);
+	public BoardDTO getOneById(int id);
+	public List<BoardDTO> getAll(Map<String, String> params);
+}
