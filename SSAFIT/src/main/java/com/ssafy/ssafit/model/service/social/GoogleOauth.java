642bb9d5b9205e1457cc3b468e0fec3d50a59576
junghun95 <hun950803@naver.com>
2022-05-24 01:35:32 +0900

    social-Login

--- SSAFIT/src/main/java/com/ssafy/ssafit/model/service/social/GoogleOauth.java
+++ SSAFIT/src/main/java/com/ssafy/ssafit/model/service/social/GoogleOauth.java
@@ -62,11 +62,11 @@ public class GoogleOauth implements SocialOauth{
         	// 토큰 받아온걸 sha256적용해서 payload에 있는 email을 읽어오자..
         	try {
 				String email = this.getEmail(responseEntity);
+				return email;
 			} catch (Exception e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
-            return responseEntity.getBody();
         }
         return "구글 로그인 요청 처리 실패";
 	}
@@ -79,7 +79,11 @@ public class GoogleOauth implements SocialOauth{
 		String payload = idToken.split("\\.")[1];
 //		System.out.println(idToken);
 		Base64.Decoder decoder = Base64.getDecoder();
-		System.out.println(new String(decoder.decode(payload), StandardCharsets.UTF_8));
-		return idToken;
+//		System.out.println(new String(decoder.decode(payload), StandardCharsets.UTF_8));
+		String strPayload = new String(decoder.decode(payload), StandardCharsets.UTF_8);
+		jsonObject = (JSONObject) jsonParse.parse(strPayload);
+		String email = (String) jsonObject.get("email");
+		System.out.println(email);
+		return email;
 	}
 }
