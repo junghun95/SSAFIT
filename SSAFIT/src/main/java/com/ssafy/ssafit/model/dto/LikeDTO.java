2fb3ad5af48177f86b753af9ac7e88f83fca810e
Garo-o <hjs8482@naver.com>
2022-05-24 04:13:02 +0900

    feature : like dto

---
+++ SSAFIT/src/main/java/com/ssafy/ssafit/model/dto/LikeDTO.java
@@ -0,0 +1,17 @@
+package com.ssafy.ssafit.model.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class LikeDTO {
+	private int userID;
+	private int reviewId;
+	private int boardId;
+	private String regDate;
+}
